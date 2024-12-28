152c02633a29cfc2ec177997b1bced4a25c91641
Zac Cobb <zacrobat55@gmail.com>
2024-12-28 20:08:24 +1100

    Update link text and add post with tags

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/cuts', title: 'cuts' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
