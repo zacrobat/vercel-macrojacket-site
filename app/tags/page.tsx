8cdbf27e55dac399982646355468abd80a315eaa
Zac Cobb <zacrobat55@gmail.com>
2025-01-14 16:41:29 +1100

    See about this error

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,5 +1,4 @@
 import Link from '@/components/Link'
-import PageTitle from '@/components/PageTitle'
 import Tag from '@/components/Tag'
 import { slug } from 'github-slugger'
 import tagData from 'app/tag-data.json'
@@ -13,7 +12,8 @@ export default async function Page() {
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
   return (
     <>
-        
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
           {/* <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
             Tags
           </h1> */}
@@ -23,8 +23,8 @@ export default async function Page() {
                     <PageTitle>Tags</PageTitle>
                 </div>
             </div>
-
-        <div className="divide-y divide-gray-200 dark:divide-gray-700 flex max-w-lg flex-wrap">
+        </div>
+        <div className="flex max-w-lg flex-wrap">
           {tagKeys.length === 0 && 'No tags found.'}
           {sortedTags.map((t) => {
             return (
@@ -41,6 +41,7 @@ export default async function Page() {
             )
           })}
         </div>
+      </div>
     </>
   )
 }
