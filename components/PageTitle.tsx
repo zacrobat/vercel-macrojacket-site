11e10dd65595f198d5e5abc4a5bd6993be8d9e1e
Zac Cobb <zacrobat55@gmail.com>
2025-01-14 17:20:26 +1100

    Fix broken tag

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -9,7 +9,7 @@ export default function PageTitle({ children }: Props) {
     // <h1 className="colortext-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
     //   {children}
     // </h1>
-    <h1 className=font-extrabold text-gray-900 dark:text-gray-100 text-4xl/tight sm:text-5xl/tight md:text-6xl/tight ">
+    <h1 className="font-extrabold text-gray-900 dark:text-gray-100 text-4xl/tight sm:text-5xl/tight md:text-6xl/tight">
       {children}
     </h1>
   )
