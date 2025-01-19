1f513533c44c02454babb51e16b6d6bfaa77a7a1
Zac Cobb <zacrobat55@gmail.com>
2025-01-20 10:57:25 +1100

    Update music page heading

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -9,6 +9,7 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import tagData from 'app/tag-data.json'
+import PageTitle from '@/components/PageTitle'
 
 interface PaginationProps {
   totalPages: number
@@ -78,21 +79,22 @@ export default function ListLayoutWithTags({
     <>
       <div>
         <div className="pb-6 pt-6">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          {/* <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
-          </h1>
+          </h1> */}
+          <PageTitle>{title}</PageTitle>
         </div>
         <div className="flex sm:space-x-24">
           <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
             <div className="px-6 py-4">
               {pathname.startsWith('/music') ? (
-                <h2 className="font-bold uppercase text-primary-500">All Music</h2>
+                <h2 className="font-bold uppercase text-primary-500">All Tags</h2>
               ) : (
                 <Link
                   href={`/music`}
                   className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
                 >
-                  All Music
+                  All Tags
                 </Link>
               )}
               <ul>
